41319fdcef00dd8ec3099febaeeeb7343829a015
TmApAZ <140719029+TmApAZ@users.noreply.github.com>
2024-12-31 18:53:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const customizationRoutes = require('./routes/customizationRoutes');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
